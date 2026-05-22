da1b87e84ccc45bdde2dbad02c7fbfc3fe28b33c
240330-Grant <240330@shakopeeschools.org>
2026-05-22 09:57:24 -0500

    idk I think it's a small amount of all of the 1.8 stuff

--- Version8.0/user52.html/weather/index.php
+++ Version8.0/user52.html/weather/index.php
@@ -1,5 +1,5 @@
 <?php
-$apiKey = "API KEY"; //You will need to add in the 
+$apiKey = "93b19598b91aef7aa72696fcfe765c67"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
 $units = "metric";//metric-Celcius  imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 

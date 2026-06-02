0f894d07840f75be5d2a318a213f8840380bd9ca
kevink1242 <234477@shakopeeschools.org>
2026-06-02 08:58:28 -0500

    cart thing works

--- Version9.0/user13.html/JAVASCRIPT/script.js
+++ Version9.0/user13.html/JAVASCRIPT/script.js
@@ -1,19 +1,43 @@
-// User created function: Shopping cart
-const cart_list = JSON.parse(localStorage.getItem('userCart')) || [];
+// Shopping cart function (User created) /////////////////////
+let cart_list = JSON.parse(localStorage.getItem('userCart')) || [];
 
-function addtoCart(itemid) {
-    cart_list.push(itemid);
+function addtoCart(selecteditem, pricelisted) {
+
+    const existingItem = cart_list.find(i => i.item === selecteditem);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart_list.push({item: selecteditem, price: pricelisted, quantity: 1});
+    }
+
+
+    
+//    for (let i = 0; i > cart_list.length; i++) {
+//        const existingItem = cart_list.find()
+//        if (selecteditem == existingItem.item) {
+//            existingItem.quantity += 1;
+//        }
+//    }
+
 
     localStorage.setItem('userCart', JSON.stringify(cart_list));
-
+    i = 0;
 }
 
+
+// Debugging functions /////////////////////
 function checkLocalStorage() {
     let current = localStorage.getItem('userCart');
-    alert(current)
+    alert(current);
 }
 
-// basic functions
+function emptyLocalStorage() {
+    localStorage.clear();
+    cart_list.length = 0;
+}
+
+// Basic functions /////////////////////
 function submitform() {
     alert('Thanks! We will look into your suggestion')
 }

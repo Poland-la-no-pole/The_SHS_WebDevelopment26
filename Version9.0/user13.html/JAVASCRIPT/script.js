bb522e5abbb6afda16e923c7c2c6ac1a9c0b012b
kevink1242 <234477@shakopeeschools.org>
2026-06-02 20:20:30 -0500

    Version 9 completed
    final additions to my version 9

--- Version9.0/user13.html/JAVASCRIPT/script.js
+++ Version9.0/user13.html/JAVASCRIPT/script.js
@@ -1,4 +1,4 @@
-// Shopping cart function (User created) /////////////////////
+//////////// Function #1: Cart Handling (API Call) ///////////////
 let cart_list = JSON.parse(localStorage.getItem('userCart')) || [];
 
 function addtoCart(selecteditem, pricelisted) {
@@ -13,49 +13,63 @@ function addtoCart(selecteditem, pricelisted) {
         cart_list.push({item: selecteditem, price: pricelisted, quantity: 1});
     }
 
-
-    
-//    for (let i = 0; i > cart_list.length; i++) {
-//        const existingItem = cart_list.find()
-//        if (selecteditem == existingItem.item) {
-//            existingItem.quantity += 1;
-//        }
-//    }
-
-
     localStorage.setItem('userCart', JSON.stringify(cart_list));
 }
 
 
-// Debugging functions /////////////////////
-function checkLocalStorage() {
+function checkLocalStorage() { // Debugging function
     let current = localStorage.getItem('userCart');
     alert(current);
     alert('list length: '+cart_list.length);
 }
 
+
+//////////////// Function #2: Emptying Local Storage/Cart /////////////////
 function emptyLocalStorage() {
+    alert('Sucessfully bought your items')
     localStorage.clear();
     cart_list.length = 0;
+    loadCart()
 }
 
-// Load Cart /////////////////////////
+////////////// Function #3: Load Cart's Contents on cart.html //////////////
 function loadCart() {
-    alert('loaded');
     let contents = document.getElementById('listdisplay');
-    const container = document.getElementById("listdisplay");
+    let totalprice = 0;
 
-    for (let i = 0; i < cart_list.length; i++) {
-        contents.innerHTML += '$'+cart_list[i].price+' Item: '+cart_list[i].item+' x'+cart_list[i].quantity;
-        if (cart_list.length > i) {
-            contents.innerHTML += '\n';
-        } else if (cart_list.length == 0) {
-            contents.innerHTML = 'Your cart is empty!';
+    if (cart_list.length > 0) {
+        for (let i = 0; i < cart_list.length; i++) {
+            contents.innerHTML += 'Item: '+cart_list[i].item+' x'+cart_list[i].quantity+' - $'+cart_list[i].price;
+            totalprice += cart_list[i].price;
+            if (cart_list.length > i) {
+                contents.innerHTML += '<br>';
+            }
         }
+        
+        contents.innerHTML += '<br>Total Price: $'+totalprice;
+    } else {
+        contents.innerHTML = 'Your cart is empty!';
+    }          
+}
+
+///////////// Function #4: Simple Alert /////////////////
+function submitform() {
+    let userinput = document.getElementById('suggestiontext');
+    
+    if (userinput.value.length < 1) {
+        alert('Fill out a suggestion before submitting!');
+    } else {
+        alert('Thanks! We will look into your suggestion');
+        userinput.value = '';
     }
 }
 
-// Basic functions /////////////////////
-function submitform() {
-    alert('Thanks! We will look into your suggestion')
+/////////////// Function #5: Changing Contents of a Container ////////////
+function checked(currentcard) {
+    currentcard.replaceChildren();
+
+    const checkmark = document.createElement('h1');
+    checkmark.textContent = 'Viewed';
+
+    currentcard.appendChild(checkmark);
 }
